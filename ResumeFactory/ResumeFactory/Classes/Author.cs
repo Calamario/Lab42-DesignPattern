1c8cc80b5a78000e4c1481634acc27fb0a2dedac
mario nishio <mario.nishio@gmail.com>
2018-08-01 23:10:54 -0700

    read me, code, and visual added

---
+++ ResumeFactory/ResumeFactory/Classes/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResumeFactory.Classes
+{
+    public abstract class Author
+    {
+        protected abstract Document WriteDocument(string type);
+        public Document SendDocument(string type)
+        {
+            Document document = WriteDocument(type);
+            document.Write();
+            return document;
+        }
+    }
+}
